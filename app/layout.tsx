e919086d2db1fe2504cb9552e342047437e51da3
Sachin Singh <sachinsingh018@gmail.com>
2026-01-06 23:23:37 -0500

    Add Google Analytics (gtag.js) to all pages via root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -145,6 +145,24 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F3KPTGYHSQ"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-F3KPTGYHSQ');
+            `,
+          }}
+        />
+        {/* End Google tag (gtag.js) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
